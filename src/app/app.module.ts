aaf90f61c67c54ea07ab5e0a0f44c5bff793a4ca
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-12-27 22:36:04 +0000

    Major project refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameComponent } from './game/game.component';
-import { SideBarComponent } from "./side-bar/side-bar.component";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GameComponent,
-    SideBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  exports: [
-    SideBarComponent
-  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
