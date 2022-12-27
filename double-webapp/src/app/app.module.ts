cf86152e887086ead579fc2810b07cc4bcd938b7
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-12-27 22:11:09 +0000

    Setting up ng components

--- double-webapp/src/app/app.module.ts
+++ double-webapp/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { SideBarComponent } from "./side-bar/side-bar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  exports: [
+    SideBarComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
