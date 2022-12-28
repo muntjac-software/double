62ab3ab8eafb7fb922b0ba80e97886e44d4fdfc5
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-12-28 16:39:30 +0000

    [minor] DBLE-17: cypress menu test

--- cypress/e2e/menu.spec.cy.ts
+++ cypress/e2e/menu.spec.cy.ts
@@ -1,8 +1,26 @@
 describe('Menu Section', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
   it('should change element visibility when menu is toggled', () => {
+    // given
+    cy.get('header > #tag-line').should('have.css', 'opacity', '0');
 
-    // TODO: implement test for tag line and sign off ticket
+    // when
+    cy.get('#menu').click();
+    cy.pause();
+
+    // then
+    cy.get('header > #tag-line').should('not.have.css', 'opacity', '0');
+
+    // when
+    cy.get('#menu').click();
+    cy.pause();
+
+    // then
+    cy.get('header > #tag-line').should('have.css', 'opacity', '0');
+
   });
 
 })
