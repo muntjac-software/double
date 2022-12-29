dabcaf28a0162acb3155e1c3f9e742f54f824adf
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-12-29 19:11:20 +0000

    [minor] DBLE-18: menu bar progress

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {faCircleInfo, faCog} from "@fortawesome/free-solid-svg-icons";
+import { Component, Input } from '@angular/core';
+import { faCircleInfo, faCog, faSkullCrossbones } from "@fortawesome/free-solid-svg-icons";
+import { Difficulty } from '../model/difficulty';
 
 @Component({
   selector: 'menu-bar',
@@ -12,5 +13,32 @@ export class MenuBarComponent {
 
   iconInfo = faCircleInfo;
   iconSettings = faCog;
+  iconDeath = faSkullCrossbones;
+
+  _difficulty: Difficulty;
+  difficultyInfo: string;
+  difficultyInfos = [ // TODO: sort properly...
+    'For those new to -le games',
+    'For regular -le game players',
+    'For those who spend too long on -le games',
+    'You will not survive'
+  ]
+
+
+
+  constructor() {
+    this._difficulty = Difficulty.RECRUIT; // TODO: tdd and extract to service?
+    this.difficultyInfo = 'For those new to -le games'; // TODO: sort properly...
+  }
+
+  get difficulty() {
+    return this._difficulty;
+  }
+
+  set difficulty(difficulty: Difficulty) {
+    this._difficulty = difficulty;
+
+    this.difficultyInfo = this.difficultyInfos[difficulty];
+  }
 
 }
---
+++ src/app/model/difficulty.ts
@@ -0,0 +1,6 @@
+export enum Difficulty {
+  RECRUIT,
+  REGULAR,
+  HARDENED,
+  VETERAN,
+}
